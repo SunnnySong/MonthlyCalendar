d961a43a13f65214d3fdbbd18584363c582923fd
SunnnySong <tjsghktjswls@naver.com>
2023-10-17 12:57:56 +0900

    feat: apply for CocoaPods

---
+++ Example/MonthlyCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonthlyCalendar
+//
+//  Created by SunnnySong on 10/17/2023.
+//  Copyright (c) 2023 SunnnySong. All rights reserved.
+//
+
+import UIKit
+import MonthlyCalendar
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
